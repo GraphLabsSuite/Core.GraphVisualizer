2d53b5bd6dbad44361fff455175366c71c2b0674
George <eakarpov@yandex.ru>
2017-09-11 17:08:16 +0300

    New exports added

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 export * from "./types/IGraphVisualizer";
+export * from "./types/Color";
+export * from "./types/IVertexVisualizer";
+export * from "./types/Point";
+
+export * from "./enums/EdgeShape";
 
 export * from "./visualizers/DefaultGraphVisualizer";
+export * from "./visualizers/CircleGraphVisualizer";
+export * from "./visualizers/CircleVertexVisualizer";
+export * from "./visualizers/DefaultVertexVisualizer";
 
 export * from "./geometrics/GeometricGraph";
 export * from "./geometrics/GeometricVertex";
