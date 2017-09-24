b6ec9797b6081a6e426616e0526fb5944c261e73
George <eakarpov@yandex.ru>
2017-09-24 15:47:20 +0300

    Graph Visualization added on 0-1 vertices

--- src/visualizers/CircleGraphVisualizer.ts
+++ src/visualizers/CircleGraphVisualizer.ts
@@ -20,29 +20,31 @@ export class CircleGraphVisualizer implements IGraphVisualizer {
      */
     public calculate(): void {
         const vertexAmount: number = this.geometric.graph.vertices.length;
-        // Calculating phi angle between two vertices
-        const phi: number = 2 * Math.PI / vertexAmount;
+        if (vertexAmount > 0) {
+            // Calculating phi angle between two vertices
+            const phi: number = 2 * Math.PI / vertexAmount;
 
-        //Approximating vertexRadius value
-        const radius1: number = (Math.min(this.width, this.height)) / 2;
-        const grandCircleLength1: number = radius1 * 2 * Math.PI;
-        const vertexRadius1: number = grandCircleLength1 / (vertexAmount * 4);
+            //Approximating vertexRadius value
+            const radius1: number = (Math.min(this.width, this.height)) / 2;
+            const grandCircleLength1: number = radius1 * 2 * Math.PI;
+            const vertexRadius1: number = grandCircleLength1 / (vertexAmount * 4);
 
-        // Get real data
-        const radius: number = (Math.min(this.width, this.height) - vertexRadius1 * 2) / 2;
-        const grandCircleLength: number = radius * 2 * Math.PI;
-        const vertexRadius: number = grandCircleLength / (vertexAmount * 4);
+            // Get real data
+            const radius: number = (Math.min(this.width, this.height) - vertexRadius1 * 2) / 2;
+            const grandCircleLength: number = radius * 2 * Math.PI;
+            const vertexRadius: number = grandCircleLength / (vertexAmount * 4);
 
-        // Calculating radius of the vertex circle (10 - default radius, 2 * 10 - diametr, x2 - between two vertices
-        // const radius: number = (vertexAmount * 4 * 10) / (2 * Math.PI);
-        const x_center = this.width / 2;
-        const y_center = this.height / 2;
-        let n = 0;
-        for (const vertex of this.geometric.graph.vertices) {
-            const y: number = radius * Math.cos(n * phi) + y_center;
-            const x: number = radius * Math.sin(n * phi) + x_center;
-            this.geometric.vertices.push(CircleVertexVisualizer.calculate(vertex, x, y, vertexRadius));
-            n++;
+            // Calculating radius of the vertex circle (10 - default radius, 2 * 10 - diametr, x2 - between two vertices
+            // const radius: number = (vertexAmount * 4 * 10) / (2 * Math.PI);
+            const x_center = this.width / 2;
+            const y_center = this.height / 2;
+            let n = 0;
+            for (const vertex of this.geometric.graph.vertices) {
+                const y: number = radius * Math.cos(n * phi) + y_center;
+                const x: number = radius * Math.sin(n * phi) + x_center;
+                this.geometric.vertices.push(CircleVertexVisualizer.calculate(vertex, x, y, vertexRadius));
+                n++;
+            }
         }
     }
 }
--- test/visualizers/CircleGraphVisualizer.test.ts
+++ test/visualizers/CircleGraphVisualizer.test.ts
@@ -11,13 +11,29 @@ describe('CircleGraphVisualizer', () => {
         graph.addVertex(vertexTwo);
         const visualizer: CircleGraphVisualizer = new CircleGraphVisualizer(graph);
 
-            it("calculate graph coordinates with 2 vertices", () => {
+        it("calculate graph coordinates with 2 vertices", () => {
             visualizer.calculate();
             chai.assert(visualizer.geometric.vertices.length == 2);
             // chai.assert(visualizer.geometric.vertices[0].center.X == 100);
             // chai.assert(Math.floor(visualizer.geometric.vertices[0].center.Y) == 112);
             // chai.assert(visualizer.geometric.vertices[1].center.X == 100);
             // chai.assert(Math.floor(visualizer.geometric.vertices[1].center.Y) == 87);
+        });
+
+        const graph1: IGraph<IVertex, IEdge> = new Graph<Vertex, Edge>();
+        const vertexOne1: IVertex = new Vertex("one", graph1);
+        graph1.addVertex(vertexOne1);
+        const visualizer1: CircleGraphVisualizer = new CircleGraphVisualizer(graph1);
+        it("Calculate graph with the single vertex", () => {
+            visualizer1.calculate();
+            chai.assert(visualizer1.geometric.vertices.length == 1);
+        });
+
+        const graph2: IGraph<IVertex, IEdge> = new Graph<Vertex, Edge>();
+        const visualizer2: CircleGraphVisualizer = new CircleGraphVisualizer(graph2);
+        it("Calculate graph with no vertices", () => {
+            visualizer2.calculate();
+            chai.assert(visualizer2.geometric.vertices.length == 0);
         });
     });
 });
